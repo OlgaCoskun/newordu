954e87f26410c349d4fb3d6222e7c83c3f045de0
Olga Coskun <ovin999@gmail.com>
2017-11-03 20:31:09 +0300

    create new template, action and route

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,4 +3,23 @@ class ArticlesController < ApplicationController
   def new
   end
 
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+      redirect_to @article
+    else
+      render action: 'new'
+    end
+  end
+
+  private
+
+    def article_params
+      params.require(:article).permit(:title, :description)
+    end
+
 end
