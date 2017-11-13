3eba90c0f94f658283277b55cb25bbd6235eaaab
Olga Coskun <ovin999@gmail.com>
2017-11-13 20:18:24 +0300

    add rulls for making new, edit, change and delete blog

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,6 @@
 class ArticlesController < ApplicationController
-  #before_action :authenticate_user!
+
+  before_action :authenticate_user!, only: [:new, :edit, :update, :destroy]
   before_action :set_article, only: [:edit, :update, :show, :destroy]
 
   def index
