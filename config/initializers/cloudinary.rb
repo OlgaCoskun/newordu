9f3b917c53fa331830132feaf21d655b3078828d
Olga Coskun <ovin999@gmail.com>
2017-11-18 23:24:30 +0300

    it works!!!)))) uploade images!!!!

--- config/initializers/cloudinary.rb
+++ config/initializers/cloudinary.rb
@@ -5,9 +5,16 @@
 #end
 
 #Cloudinary.config do |config|
- #   config.cloud_name = ENV['CLOUDINARY_CLOUD_NAME']
-  #  config.api_key = ENV['CLOUDINARY_API_KEY']
-   # config.api_secret = ENV['CLOUDINARY_API_SECRET']
-    #config.cdn_subdomain = true
-    #config.enhance_image_tag = true
+#    config.cloud_name = ENV['CLOUDINARY_CLOUD_NAME']
+#    config.api_key = ENV['CLOUDINARY_API_KEY']
+#    config.api_secret = ENV['CLOUDINARY_API_SECRET']
+#    config.cdn_subdomain = true
+#    config.enhance_image_tag = true
 #end
+
+Cloudinary.config do |config|
+  config.cloud_name = 'cloudolga'
+  config.api_key = '144655146287369'
+  config.api_secret = 'f9vbfzfxtnE1K1gkO0YKL8Janjw'
+  config.cdn_subdomain = true
+end
