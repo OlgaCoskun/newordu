1571d54c886d977c44450fc438a14c1845a51471
Olga Coskun <ovin999@gmail.com>
2017-11-25 14:39:01 +0300

    fix galeri

--- app/controllers/galeris_controller.rb
+++ app/controllers/galeris_controller.rb
@@ -1,5 +1,6 @@
 class GalerisController < ApplicationController
- before_action :set_galeri, only: [:show, :edit, :update, :destroy]
+  #before_action :authenticate_user!, only: [:new, :edit, :update, :destroy]
+  before_action :set_galeri, only: [:show, :edit, :update, :destroy]
 
   def index
     @galeris = Galeri.all
