f78c7d112376f23a1c893728ea7e76812ed03b45
Olga Coskun <ovin999@gmail.com>
2017-12-04 19:18:12 +0300

    fix name for Users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  #before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :configure_permitted_parameters, if: :devise_controller? # for add name of people to the Users.name
 
-  #private
+  private
 
-  #def configure_permitted_parameters
-  #  devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
-  #end
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
 end
 
