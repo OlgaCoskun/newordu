88ba2dc8e223c5c6c61e60c1188d2d38352602e1
Olga Coskun <ovin999@gmail.com>
2017-12-09 21:20:11 +0300

    refactoring

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,8 @@
 Rails.application.routes.draw do
 
   devise_for :users
-  #get 'home/index'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
-  #root to: "home#index"
 
   root 'welcome#anasayfa'
-  #get 'yonetim', to: 'welcome#yonetim'
 
   scope(path_names: {new: 'yeni', edit: 'değiştermek'}) do
     resources :articles,  path: 'blog'
@@ -18,14 +12,7 @@ Rails.application.routes.draw do
     resources :galeris,  path: 'galeri'
   end
 
-  #root 'galeris#index'
-  #resources :galeris
-
-  #get 'iletisim/index'
   get 'iletisim', to: 'iletisim#index'
-
-  #get 'duyurular/index'
-  #get 'duyurular', to: 'duyurular#index'
 
   scope(path_names: {new: 'yeni', edit: 'değiştermek'}) do
     resources :duyurulars,  path: 'duyurular'
